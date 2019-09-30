d02614c653b561b88d8db83555415143d98ac124
eeeric3995@yahoo.com <ihxx2s7tbu0>
2019-09-30 16:39:53 -0400

    Saving Lab

---
+++ Save_lab/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  Save_lab
+//
+//  Created by EricM on 9/30/19.
+//  Copyright © 2019 EricM. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Save_lab/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Save_lab
+//
+//  Created by EricM on 9/30/19.
+//  Copyright © 2019 EricM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
